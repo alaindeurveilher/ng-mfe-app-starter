ed4563ea8c66791ebdf9e294fcb38ad7815b542b
Alain D'eurveilher <alain.deurveilher@infrabel.be>
2024-02-23 23:18:02 +0100

    add Angular Native Federation and configure it

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'mfe1', loadComponent: () => loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent) },
+  { path: 'mfe2', loadComponent: () => loadRemoteModule('mfe2', './Component').then((m) => m.AppComponent) },
   { path: '**', component: NotFoundComponent },
   // DO NOT insert routes after this one.
   // { path:'**', ...} needs to be the LAST one.
