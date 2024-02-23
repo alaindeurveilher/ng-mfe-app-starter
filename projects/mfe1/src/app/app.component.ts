03d5a88378b7fa89c6f7fb130eba8ead545f81df
Alain D'eurveilher <alain.deurveilher@infrabel.be>
2024-02-23 23:34:28 +0100

    Added features and navigation to mfe1 and mfe2

--- projects/mfe1/src/app/app.component.ts
+++ projects/mfe1/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { Feat1Component } from './feat1/feat1.component';
+import { Feat2Component } from './feat2/feat2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, Feat1Component, Feat2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mfe1';
+  protected displayedFeature: WritableSignal<'one' | 'two'> = signal<'one' | 'two'>('one');
+
+  protected onFeatureChange(feature: 'one' | 'two'): void {
+    this.displayedFeature.set(feature);
+  }
 }
--- projects/mfe2/src/app/app.routes.ts
+++ projects/mfe2/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+];
