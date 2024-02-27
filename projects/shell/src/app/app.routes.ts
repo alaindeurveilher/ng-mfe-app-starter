bb974ffac9e2e0c579f2363cf7506d70f581bbdd
Alain D'eurveilher <alain.deurveilher@infrabel.be>
2024-02-27 11:04:04 +0100

    Add another external MFE

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -7,6 +7,7 @@ export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', title: 'Shell' },
   { path: 'mfe1', loadComponent: () => loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent), title: 'MFE 1' },
   { path: 'mfe2', loadChildren: () => loadRemoteModule('mfe2', './Module').then((m) => m.MainModule), title: 'MFE 2' },
+  { path: 'mfe3', loadChildren: () => loadRemoteModule('mfe3', './Module').then((m) => m.MainModule), title: 'MFE 3 ext.' },
   { path: '**', component: NotFoundComponent, title: 'Page Not Found' },
   // DO NOT insert routes after this one.
   // { path:'**', ...} needs to be the LAST one.
